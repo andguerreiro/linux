c34c193f9b9822ec45a3d9464828756fe30ffa79
Andrews Guerreiro <104579932+andguerreiro@users.noreply.github.com>
2026-01-31 08:56:33 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -24,22 +24,4 @@ mkdir -p ~/.config/pipewire/pipewire.conf.d/
 echo 'context.properties = { default.clock.allowed-rates = [ 44100 48000 96000 192000 ] }' > ~/.config/pipewire/pipewire.conf.d/custom-rates.conf
 systemctl --user restart pipewire
 
-# 6. Install Software
-sudo apt install -y mpv qbittorrent libreoffice-calc libreoffice-gnome
-sudo snap install spotify gimp
-
-# 7. MPV with NVIDIA GPU
-mkdir -p ~/.config/mpv/
-cat <<EOF > ~/.config/mpv/mpv.conf
-vo=gpu
-gpu-api=opengl
-hwdec=nvdec
-profile=gpu-hq
-scale=ewa_hanning
-cscale=ewa_hanning
-video-sync=display-resample
-interpolation
-tscale=oversample
-EOF
-
 echo "Setup complete! Please reboot for all changes to take effect."
