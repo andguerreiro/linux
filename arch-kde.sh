5da4cee1b35ba8575876b96c67f42a433f77fdea
andmorgue <andguerreiro@yahoo.com>
2026-04-24 16:44:44 -0300

    Update arch-kde.sh

--- arch-kde.sh
+++ arch-kde.sh
@@ -24,10 +24,15 @@ mkdir -p ~/.config/pipewire/pipewire.conf.d/
 echo 'context.properties = { default.clock.allowed-rates = [ 44100 48000 96000 192000 ] }' > ~/.config/pipewire/pipewire.conf.d/custom-rates.conf
 systemctl --user restart pipewire pipewire-pulse wireplumber || true
 
-# 5. Hardware Rules (Razer Huntsman v3)
-sudo tee /etc/udev/rules.d/99-razer-huntsman-v3.rules <<EOF >/dev/null
+# 5. Hardware Rules (Razer & VXE/ATK)
+sudo tee /etc/udev/rules.d/99-peripherals.rules <<EOF >/dev/null
+# Razer Huntsman v3 pro mini
 ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1532", MODE="0666", GROUP="wheel"
 ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1532", MODE="0666", GROUP="wheel"
+
+# VXE / ATK (Mouse WebHID)
+ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
+ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
 EOF
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
