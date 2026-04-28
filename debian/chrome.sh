044ecdd1cb7c14db76286df818ca2d93221c733e
andmorgue <andguerreiro@yahoo.com>
2026-04-28 06:57:14 -0300

    Update chrome.sh

--- debian/chrome.sh
+++ debian/chrome.sh
@@ -11,26 +11,35 @@ sudo apt update
 sudo apt install -y /tmp/google-chrome.deb
 echo "Google Chrome installation complete."
 
-# 3. Create Udev rule for Razer Huntsman V3
-echo "--- Configuring hardware permissions (udev) ---"
+# 3. Razer Huntsman V3 udev rule
+echo "--- Configuring Razer hardware permissions (udev) ---"
 sudo bash -c 'cat <<EOF > /etc/udev/rules.d/99-razer-huntsman-v3.rules
 # Razer Huntsman V3 - WebHID Access
 KERNEL=="hidraw*", ATTRS{idVendor}=="1532", MODE="0666", TAG+="uaccess"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="1532", MODE="0666", TAG+="uaccess"
 EOF'
-echo "Udev rules created for Razer hardware."
+echo "Razer udev rules created."
 
-# 4. Reload system rules and trigger changes
+# 4. VXE / ATK Mouse WebHID udev rule
+echo "--- Configuring VXE/ATK hardware permissions (udev) ---"
+sudo bash -c 'cat <<EOF > /etc/udev/rules.d/99-vxe-atk-mouse.rules
+# VXE / ATK (Mouse WebHID)
+ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3554", MODE="0666", TAG+="uaccess"
+ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="3554", MODE="0666", TAG+="uaccess"
+EOF'
+echo "VXE/ATK udev rules created."
+
+# 5. Apply udev rules
 echo "--- Applying new hardware rules ---"
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 echo "Hardware rules reloaded and triggered."
 
-# 5. Cleanup temporary files
+# 6. Cleanup temporary files
 echo "--- Cleaning up ---"
 rm /tmp/google-chrome.deb
 echo "Temporary installation files removed."
 
 echo "-----------------------"
 echo "--- SETUP COMPLETE ---"
-echo "Tip: Make sure to restart Chrome if it was already open."
+echo "Tip: Restart Chrome if it was already open."
