6903baf625c3e09f9ee2005ec61a76f3976b6c34
andmorgue <andguerreiro@yahoo.com>
2026-05-05 18:42:12 -0300

    Update kde.sh

--- arch/kde.sh
+++ arch/kde.sh
@@ -24,19 +24,7 @@ mkdir -p ~/.config/pipewire/pipewire.conf.d/
 echo 'context.properties = { default.clock.allowed-rates = [ 44100 48000 96000 192000 ] }' > ~/.config/pipewire/pipewire.conf.d/custom-rates.conf
 systemctl --user restart pipewire pipewire-pulse wireplumber || true
 
-# 5. Hardware Rules (Razer & VXE/ATK)
-sudo tee /etc/udev/rules.d/99-peripherals.rules <<EOF >/dev/null
-# Razer Huntsman v3 pro mini
-ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1532", MODE="0666", GROUP="wheel"
-ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1532", MODE="0666", GROUP="wheel"
-
-# VXE / ATK (Mouse WebHID)
-ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
-ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
-EOF
-sudo udevadm control --reload-rules && sudo udevadm trigger
-
-# 6. Boot Configuration (Timeout 0)
+# 5. Boot Configuration (Timeout 0)
 if [ -f /boot/loader/loader.conf ]; then
     sudo sed -i '/^#* *timeout/d' /boot/loader/loader.conf
     echo "timeout 0" | sudo tee -a /boot/loader/loader.conf
