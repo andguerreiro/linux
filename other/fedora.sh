491587b50ac06922bdaf3c0afa3588ffb2798b11
andmorgue <andguerreiro@yahoo.com>
2026-05-16 11:20:59 -0300

    Update fedora.sh

--- other/fedora.sh
+++ other/fedora.sh
@@ -45,6 +45,7 @@ sudo systemctl disable --now bluetooth.service
 echo ">> Applying GNOME settings..."
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
+gsettings set org.gnome.SessionManager logout-prompt false
 
 # Final system upgrade & cleanup
 echo ">> Final upgrade and cleanup..."
