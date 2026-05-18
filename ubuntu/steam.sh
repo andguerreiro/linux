8cbdcc6c049421a602586340822eb3a51e1eefee
andmorgue <andguerreiro@yahoo.com>
2026-05-18 08:43:30 -0300

    Create steam.sh

---
+++ ubuntu/steam.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd /tmp
+wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
+sudo apt update
+sudo apt install -y ./steam_latest.deb
+rm -f ./steam_latest.deb
