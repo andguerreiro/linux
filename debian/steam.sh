7d3b7ea2bca196ce5592787587aa4aafee0bb53a
andmorgue <andguerreiro@yahoo.com>
2026-05-18 08:49:35 -0300

    Update steam.sh

--- debian/steam.sh
+++ debian/steam.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 cd /tmp
 wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
 sudo apt update
-sudo apt install -y ./steam_latest.deb
+sudo apt install -y ./steam.deb
 rm -f ./steam_latest.deb
