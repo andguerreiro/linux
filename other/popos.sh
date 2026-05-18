0ec82c6423c502205c5b85b68ca95ea1b16844fd
andmorgue <andguerreiro@yahoo.com>
2026-05-18 10:50:39 -0300

    Update popos.sh

--- other/popos.sh
+++ other/popos.sh
@@ -1,6 +1,7 @@
 # Update and cleanup system
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && \
+sudo apt update && sudo apt upgrade -y && \
 sudo apt remove --purge thunderbird vim -y && \
+sudo apt autoremove -y
 
 # Audio optimization (Sample Rates)
 mkdir -p ~/.config/pipewire/pipewire.conf.d/ && \
