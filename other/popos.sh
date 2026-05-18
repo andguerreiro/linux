08ab524f2ad2a264926212f7b7577a8684df018b
andmorgue <andguerreiro@yahoo.com>
2026-05-18 10:44:59 -0300

    Update and rename pop.sh to popos.sh

---
+++ other/popos.sh
@@ -0,0 +1,8 @@
+# Update and cleanup system
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && \
+sudo apt remove --purge thunderbird vim -y && \
+
+# Audio optimization (Sample Rates)
+mkdir -p ~/.config/pipewire/pipewire.conf.d/ && \
+echo 'context.properties = { default.clock.allowed-rates = [ 44100 48000 88200 96000 ] }' > ~/.config/pipewire/pipewire.conf.d/custom-rates.conf && \
+systemctl --user restart pipewire
