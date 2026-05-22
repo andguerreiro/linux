44ee4adf2bcdac5655a6a6724d8c4dcf2c6e15c7
andmorgue <andguerreiro@yahoo.com>
2026-05-22 08:07:58 -0300

    Create pipewire.sh

---
+++ arch/pipewire.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
+mkdir -p ~/.config/pipewire/pipewire.conf.d/
+echo 'context.properties = { default.clock.allowed-rates = [ 44100 48000 96000 192000 ] }' > ~/.config/pipewire/pipewire.conf.d/custom-rates.conf
+systemctl --user restart pipewire pipewire-pulse wireplumber || true
