38615a35f0e1b3db281d21357d68ee7d80c7881f
andmorgue <andguerreiro@yahoo.com>
2026-05-22 17:01:49 -0300

    Update kde.sh

--- arch/kde.sh
+++ arch/kde.sh
@@ -6,13 +6,9 @@ sudo pacman -S --needed --noconfirm \
     dolphin kate ark gwenview kcalc okular \
     unrar zip unzip ufw wireless-regdb power-profiles-daemon
 
-# Boot Security (fstab & permissions)
-# Replaces existing options with secure masks (0077)
-sudo sed -i -E '/\/boot/ s/(vfat\s+)\S+/\1rw,relatime,fmask=0077,dmask=0077/' /etc/fstab
-sudo systemctl daemon-reload
-sudo umount /boot || true
-sudo mount /boot
-sudo bootctl random-seed
+# Package Removal
+sudo pacman -Rns --noconfirm vim
+sudo pacman -Rdd --noconfirm discover
 
 # Boot Configuration (Timeout 0)
 echo "timeout 0" | sudo tee /boot/loader/loader.conf
