b1c3a1c67d0c2e8337e9a8ebfe2c751406602be1
andmorgue <andguerreiro@yahoo.com>
2026-05-30 09:25:54 -0300

    Create vxe.sh

---
+++ steamos/vxe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+sudo tee /etc/udev/rules.d/99-vxe.rules <<EOF
+ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
+ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
+EOF
+
+sudo udevadm control --reload-rules && sudo udevadm trigger
+echo "Done!"
