ff606b792fa85fb96d71cadeee1b89faa1f71250
andmorgue <andguerreiro@yahoo.com>
2026-05-30 09:22:19 -0300

    Update and rename webHID.sh to razer.sh

--- arch/razer.sh
+++ arch/razer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-sudo tee /etc/udev/rules.d/99-gaming.rules <<EOF
+
+sudo tee /etc/udev/rules.d/99-razer.rules <<EOF
 KERNEL=="hidraw*", ATTRS{idVendor}=="1532", MODE="0666", GROUP="wheel"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="1532", MODE="0666", GROUP="wheel"
-ACTION=="add", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
-ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="3554", MODE="0666", GROUP="wheel"
 EOF
+
 sudo udevadm control --reload-rules && sudo udevadm trigger
 echo "Done!"
