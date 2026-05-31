e9cf06e1d830709f0e4bbe1970243872b26558af
andmorgue <andguerreiro@yahoo.com>
2026-05-31 09:59:39 -0300

    Update and rename webHID.sh to razer.sh

--- debian/razer.sh
+++ debian/razer.sh
@@ -8,9 +8,5 @@ sudo tee /etc/udev/rules.d/99-razer-webhid.rules >/dev/null <<'EOF'
 KERNEL=="hidraw*", ATTRS{idVendor}=="1532", MODE="0660", GROUP="plugdev"
 EOF
 
-sudo tee /etc/udev/rules.d/99-vxe-webhid.rules >/dev/null <<'EOF'
-KERNEL=="hidraw*", ATTRS{idVendor}=="3554", MODE="0660", GROUP="plugdev"
-EOF
-
 sudo udevadm control --reload-rules
 sudo udevadm trigger
