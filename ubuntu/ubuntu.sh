7fe19466a2c1029e60e7b840e9d00aecc9ce5262
andmorgue <andguerreiro@yahoo.com>
2026-06-02 20:04:50 -0300

    Update ubuntu.sh

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -8,3 +8,4 @@ sudo systemctl disable --now bluetooth.service
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.SessionManager logout-prompt false
+gsettings set org.gnome.desktop.interface enable-animations false
