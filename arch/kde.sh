339210dfb01ab452d511cebb80dcfdecfa397bc0
andmorgue <andguerreiro@yahoo.com>
2026-06-15 20:28:14 -0300

    Update kde.sh

--- arch/kde.sh
+++ arch/kde.sh
@@ -13,8 +13,4 @@ sudo pacman -Rdd --noconfirm discover
 # Boot Configuration (Timeout 0)
 echo "timeout 0" | sudo tee /boot/loader/loader.conf
 
-# Wi-Fi Power Management & Stability Fixes
-echo "options mt76x2u disable_usb_lpm=1" | sudo tee /etc/modprobe.d/mt76x2u.conf
-sudo mkdir -p /etc/NetworkManager/conf.d && echo -e "[connection]\nwifi.powersave = 2" | sudo tee /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
-
 echo "System updated and secured. Reboot recommended."
