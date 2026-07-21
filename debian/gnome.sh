facc3a856817c29ce24e6318206436c1bb749f1b
andguerreiro <andguerreiro@yahoo.com>
2026-07-21 06:10:33 -0300

    Update gnome.sh

--- debian/gnome.sh
+++ debian/gnome.sh
@@ -31,4 +31,3 @@ echo "[GNOME] Applying desktop tweaks..."
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.SessionManager logout-prompt false
-gsettings set org.gnome.desktop.interface enable-animations false
