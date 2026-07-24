c6c57ecc5e0ae40210c5f3b50e030e7313c7a98d
andguerreiro <andguerreiro@yahoo.com>
2026-07-24 11:48:23 -0300

    Update workstation.sh

--- fedora/workstation.sh
+++ fedora/workstation.sh
@@ -49,7 +49,6 @@ echo ">> Applying GNOME settings..."
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.SessionManager logout-prompt false
-gsettings set org.gnome.desktop.interface enable-animations false
 
 # Final cleanup
 echo ">> Final cleanup..."
