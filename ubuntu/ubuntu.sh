5518587d00af3fa883d537c40958a6042858ea6e
andguerreiro <andguerreiro@yahoo.com>
2026-08-01 10:36:59 -0300

    Update ubuntu.sh

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-# Disable Bluethooth
 sudo systemctl disable --now bluetooth.service
-
-# GNOME Desktop Tweaks
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.SessionManager logout-prompt false
+
 gsettings set org.gnome.mutter center-new-windows true
