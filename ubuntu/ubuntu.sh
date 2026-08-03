f72e370ddac4abeb089d506b278f7fd4fc5f4677
andguerreiro <andguerreiro@yahoo.com>
2026-08-03 18:06:55 -0300

    Update ubuntu.sh

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo systemctl disable --now bluetooth.service
+sudo systemctl disable --now bluetooth
+sudo systemctl disable --now ModemManager
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.SessionManager logout-prompt false
