3071288872cd6059c91a966290fb8133bbc8cff9
andguerreiro <andguerreiro@yahoo.com>
2026-08-15 17:49:31 -0300

    Update xfce.sh

--- arch/xfce.sh
+++ arch/xfce.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+sudo pacman -Rns --noconfirm vim
+
 echo "timeout 0" | sudo tee /boot/loader/loader.conf
 
 mkdir -p ~/.config/pipewire/pipewire.conf.d/
@@ -37,90 +39,80 @@ cat > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml <<'EOF'
         <value type="int" value="1"/>
         <value type="int" value="2"/>
         <value type="int" value="3"/>
-        <value type="int" value="6"/>
+        <value type="int" value="14"/>
         <value type="int" value="5"/>
         <value type="int" value="8"/>
         <value type="int" value="12"/>
       </property>
     </property>
   </property>
-
   <property name="plugins" type="empty">
-
     <property name="plugin-2" type="string" value="tasklist">
       <property name="grouping" type="uint" value="1"/>
       <property name="show-labels" type="bool" value="true"/>
     </property>
-
     <property name="plugin-3" type="string" value="separator">
       <property name="expand" type="bool" value="true"/>
       <property name="style" type="uint" value="0"/>
     </property>
-
-    <property name="plugin-6" type="string" value="systray">
-      <property name="square-icons" type="bool" value="true"/>
-      <property name="known-items" type="array">
-        <value type="string" value="steam"/>
-      </property>
-    </property>
-
     <property name="plugin-8" type="string" value="clock">
       <property name="digital-layout" type="uint" value="3"/>
       <property name="digital-time-format" type="string" value="%d %b, %H:%M"/>
       <property name="digital-time-font" type="string" value="Sans 12"/>
     </property>
-
     <property name="plugin-9" type="string" value="separator">
       <property name="style" type="uint" value="0"/>
     </property>
-
     <property name="plugin-5" type="string" value="pulseaudio">
       <property name="enable-keyboard-shortcuts" type="bool" value="true"/>
       <property name="volume-step" type="uint" value="1"/>
       <property name="known-players" type="string" value="firefox"/>
     </property>
-
     <property name="plugin-11" type="string" value="whiskermenu">
       <property name="recent" type="array">
-        <value type="string" value="firefox.desktop"/>
+        <value type="string" value="xfce4-about.desktop"/>
+        <value type="string" value="xfce-ui-settings.desktop"/>
         <value type="string" value="steam.desktop"/>
-        <value type="string" value="org.xfce.mousepad.desktop"/>
+        <value type="string" value="kitty.desktop"/>
         <value type="string" value="xfce-keyboard-settings.desktop"/>
+        <value type="string" value="firefox.desktop"/>
+        <value type="string" value="org.xfce.mousepad.desktop"/>
       </property>
-
       <property name="favorites" type="array">
         <value type="string" value="xfce4-web-browser.desktop"/>
         <value type="string" value="xfce4-file-manager.desktop"/>
         <value type="string" value="xfce4-terminal-emulator.desktop"/>
       </property>
-
       <property name="show-command-restart" type="bool" value="true"/>
       <property name="show-command-shutdown" type="bool" value="true"/>
       <property name="confirm-session-command" type="bool" value="false"/>
     </property>
-
     <property name="plugin-1" type="string" value="launcher">
       <property name="items" type="array">
-        <value type="string" value="xfce4-web-browser.desktop"/>
+        <value type="string" value="17868253773.desktop"/>
       </property>
     </property>
-
     <property name="plugin-4" type="string" value="launcher">
       <property name="items" type="array">
-        <value type="string" value="xfce4-file-manager.desktop"/>
+        <value type="string" value="17868253772.desktop"/>
       </property>
     </property>
-
     <property name="plugin-10" type="string" value="launcher">
       <property name="items" type="array">
-        <value type="string" value="xfce4-terminal-emulator.desktop"/>
+        <value type="string" value="17868253771.desktop"/>
       </property>
     </property>
-
     <property name="plugin-12" type="string" value="separator">
       <property name="style" type="uint" value="0"/>
     </property>
-
+    <property name="plugin-14" type="string" value="systray">
+      <property name="known-legacy-items" type="array">
+        <value type="string" value="steam"/>
+      </property>
+      <property name="known-items" type="array">
+        <value type="string" value="steam"/>
+      </property>
+    </property>
   </property>
 </channel>
 EOF
