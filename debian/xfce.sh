41a508965afa64487783f6525db29fcbecb48241
andguerreiro <andguerreiro@yahoo.com>
2026-08-18 06:47:08 -0300

    Update xfce.sh

--- debian/xfce.sh
+++ debian/xfce.sh
@@ -8,21 +8,6 @@ sudo sed -i \
 
 sudo update-grub
 
-sudo systemctl disable --now bluetooth.service 2>/dev/null || true
-sudo systemctl disable --now ModemManager.service 2>/dev/null || true
-
-mkdir -p ~/.config/pipewire/pipewire.conf.d/
-
-printf '%s\n' \
-'context.properties = {' \
-'    default.clock.allowed-rates = [ 44100 48000 96000 192000 ]' \
-'}' \
-> ~/.config/pipewire/pipewire.conf.d/custom-rates.conf
-
-systemctl --user restart pipewire pipewire-pulse wireplumber
-
-rm -f ~/.config/autostart/xfce4-screensaver.desktop
-
 xfconf-query -c xfce4-power-manager \
   -p /xfce4-power-manager/presentation-mode \
   --create -t bool -s true
