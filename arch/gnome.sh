5f04f3ef0eb2904b905479775f4b272795cc6f11
andguerreiro <andguerreiro@yahoo.com>
2026-08-23 20:07:36 -0300

    Update gnome.sh

--- arch/gnome.sh
+++ arch/gnome.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 # Boot Configuration
 sudo sed -i 's/^timeout .*/timeout 0/' /boot/loader/loader.conf
 
-echo "System updated and secured. Reboot recommended."
+# GNOME Desktop Tweaks
+gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
+gsettings set org.gnome.SessionManager logout-prompt false
+
+echo "Done. Reboot recommended."
