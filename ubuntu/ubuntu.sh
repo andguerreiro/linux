479b7fdd223909a5f149e5500459b431efcd9e67
andguerreiro <andguerreiro@yahoo.com>
2026-09-14 10:07:31 -0300

    Update ubuntu.sh

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -3,8 +3,9 @@ set -e
 
 sudo systemctl disable --now bluetooth
 sudo systemctl disable --now ModemManager
+
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/gnome-printers-panel/ enable false
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 gsettings set org.gnome.SessionManager logout-prompt false
 
-gsettings set org.gnome.mutter center-new-windows true
+sudo apt install -y python3-nautilus && mkdir -p ~/.local/share/nautilus-python/extensions && printf '%s\n' 'from gi.repository import Nautilus, GObject' 'import subprocess' '' 'class OpenInTextEditor(GObject.GObject, Nautilus.MenuProvider):' '    def get_file_items(self, files):' '        if not files:' '            return []' '        item = Nautilus.MenuItem(name="OpenInTextEditor::Open", label="Open in Text Editor")' '        item.connect("activate", self.open_files, files)' '        return [item]' '' '    def get_background_items(self, current_folder):' '        return []' '' '    def open_files(self, item, files):' '        for file in files:' '            if file.get_uri_scheme() == "file":' '                subprocess.Popen(["gnome-text-editor", file.get_location().get_path()])' > ~/.local/share/nautilus-python/extensions/open_in_text_editor.py && nautilus -q
